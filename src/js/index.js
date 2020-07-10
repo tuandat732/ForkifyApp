76cbdf5c2a470b0e456be265a53b542cd270ee78
tuandat732 <spt.may7320@gmail.com>
2020-07-10 09:28:59 +0700

    add babel and test api

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 // Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+    try{
+    const res = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes)
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+getResults('pizza');
+
+// 462b1cc8d4f2730081462fbc65136320
+// http://food2fork.com/api/search
+// https://forkify-api.herokuapp.com/api
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+/*
+    In webpack, there are four core concepts:
+        + entry point
+        + output
+        + loader
+        + plugins
+
+    - Using webpack => can import/ export in js file
+*/
+
 module.exports = {
-    entry: './src/js/index.js', // entry: define file bắt đầu
-    output: { // define đầu ra
+    entry: ['@babel/polyfill', './src/js/index.js'], // entry: where webpack will start the bundle
+    // polyfile: có những code là của riêng es6+ mà es5 ko có => ko convert dc => polyfile để thêm các code es6+ vào mà es5 ko có
+    output: { // define đầu ra - absolute path
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js',
     },
@@ -11,13 +22,27 @@ module.exports = {
         còn khi mode ở chế độ production thì webpack sẽ nén file lại, minifly code,... để tối ưu hơn        
     */
     devServer: {
-        contentBase : './dist',
+        contentBase : './dist', // đường dẫn để webpack-server chạy file
     },
     plugins: [ // plugin html này để tự động cop code html từ thư mục src sang dist và thêm thẻ script dẫn dến bundle.js
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+
+    /*
+        Loaders in webpack allow to import or load all kinds of different files. And more importantly
+        to also process them. Like converting SASS to CSS or ES6+ to ES5
+    */
+   module: {
+       rules: [{
+           test: /\.js$/, // test all js file for change ES6+ to 5
+           exclude: /node_modules/, 
+           use: {
+               loader: 'babel-loader',
+           }
+       }]
+   }
 }
 
